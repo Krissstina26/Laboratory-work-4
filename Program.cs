4fb21c8764dfa2ac818a8c04c07f5bfabcb9a602
Krissstina26 <162923791+Krissstina26@users.noreply.github.com>
2026-01-12 19:06:19 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WellnessApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellness Tracker Application");
+        }
+    }
+}
